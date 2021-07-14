b6adb8fdd5e793cbef7e2beb2dc46acee4659816
christaddeucci <chris.taddeucci@gmail.com>
2021-07-13 21:39:10 -0500

    added documentation for demo.py

--- demo.py
+++ demo.py
@@ -1,3 +1,15 @@
 # create virtual env (windows cmd) - py -m venv env
 # start virtual env (windows cmd) - env\Scripts\activate
 # set app environment (windows cmd) - set FLASK_APP=app.py
+# set app debug (windows cmd) - set FLASK_DEBUG=1
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>hello world</h1>' # returns string to webpage
+    
+    
